5105c50227732a16dc58fe5b0238984f63faf741
betancito <jeronimobeta90@gmail.com>
2024-11-01 11:38:51 -0500

    fix: we add the routes necessary for the operation of the controller

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\ReserveController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::resource('reserves', ReserveController::class);
 Route::get('/', function () {
     return view('welcome');
 });
