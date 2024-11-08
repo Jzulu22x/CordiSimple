606ff3cbe09b6abd6b72d5302491056dafea0619
betancito <jeronimobeta90@gmail.com>
2024-11-08 10:47:12 -0500

    fix: adaptation for administrator and normal user views

--- app/Http/Controllers/ReserveController.php
+++ app/Http/Controllers/ReserveController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use App\Models\User;
 use App\Models\Reserve;
+use App\Models\Status;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReserveController extends Controller
 {
@@ -13,9 +16,17 @@ class ReserveController extends Controller
      */
     public function index()
     {
-        $reserves = Reserve::all();
+        if (Auth::check()) {
+            $user = Auth::user();
 
-        return view('reserves.index', compact('reserves'));
+            if ($user->roles_id == '2') {
+                $reserves = Reserve::all();
+                return view('reserves.admin.index', compact('reserves'));
+            } else {
+                $reserves = Reserve::where('user_id', $user->id)->get();
+                return view('reserves.user.index', compact('reserves'));
+            }
+        }
     }
 
     /**
@@ -23,8 +34,19 @@ class ReserveController extends Controller
      */
     public function create()
     {
-        $events = Event::all(); 
-        return view('reserves.create', compact('events'));
+        if (Auth::check()) {
+            $user = Auth::user();
+
+            if ($user->roles_id == '2') {
+                $events = Event::all();
+                $users = User::all();
+                $statuses = Status::all();
+                return view('reserves.admin.create', compact('events', 'users', 'statuses'));
+            } else {
+                $events = Event::all();
+                return view('reserves.user.create', compact('events')); 
+            }
+        }
     }
 
     /**
@@ -32,7 +54,6 @@ class ReserveController extends Controller
      */
     public function store(Request $request)
     {
-
 
         $validatedData = $request->validate([
             'event_id' => 'required|integer',
@@ -46,7 +67,17 @@ class ReserveController extends Controller
         $reserve->status_id = $validatedData['status_id'];
         $reserve->save();
 
-        return redirect()->route('reserves.index')->with('success', 'Reserva creada exitosamente.');
+        if (Auth::check()) {
+            $user = Auth::user();
+
+            if ($user->roles_id == '2') {
+                return redirect()->route('reserves.admin.index')->with('success', 'Reserva creada exitosamente.');
+            } else {
+                $events = Event::all();
+                return redirect()->route('reserves.user.index')->with('success', 'Reserva creada exitosamente.');
+            }
+        }
+        
     }
 
     /**
@@ -56,11 +87,51 @@ class ReserveController extends Controller
     {
         $ReserveDetails = Reserve::find($id);
 
-        if(!$ReserveDetails){
-            return redirect()->route('reserves.index')->with('error','Category not found');
+        if (!$ReserveDetails) {
+            return redirect()->route('reserves.index')->with('error', 'Category not found');
         }
 
-        return view('reserves.show', compact('ReserveDetails'));
+        return view('reserves.user.show', compact('ReserveDetails'));
+    }
+
+    public function edit(string $id)
+    {
+        $events = Event::all();
+        $users = User::all();
+        $statuses = Status::all();
+        $reserveEdit = Reserve::findOrFail($id);
+        return view('reserves.admin.edit', compact('reserveEdit','events', 'users', 'statuses'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $user = Auth::user();
+        $reserve = Reserve::find($id);
+        
+        if (!$reserve) {
+
+            if ($user->roles_id == '2') {
+                return redirect()->route('reserves.admin.index')->with('error', 'Category not found');
+            } else {
+                $events = Event::all();
+                return redirect()->route('reserves.user.index')->with('error', 'Category not found');
+            }
+        }
+
+        $reserve->event_id = $request -> event_id;
+        $reserve->user_id = $request -> user_id;
+        $reserve->status_id = $request -> status_id;
+        $reserve->save();
+
+        if ($user->roles_id == '2') {
+            return redirect()->route('reserves.admin.index')->with('success', 'Reserva editada exitosamente.');
+        } else {
+            $events = Event::all();
+            return redirect()->route('reserves.user.index')->with('success', 'Reserva editada exitosamente.');
+        }
     }
 
     /**
